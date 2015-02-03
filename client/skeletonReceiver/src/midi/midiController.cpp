ca6db7aade154249fc3317b4855b21a8825816f5
Dan Moore <dan@makeitdoathing.com>
2015-02-02 20:50:44 -0500

    commenting out sequencer out

--- client/skeletonReceiver/src/midi/midiController.cpp
+++ client/skeletonReceiver/src/midi/midiController.cpp
@@ -65,5 +65,5 @@ void midiController::draw(){
 
 void midiController::triggerMidiNote(int channel, int note, int velocity){
     midiOut.sendNoteOn(channel, note, velocity);
-    ofLog()<<
+    ofLog()<<"trigger midi"<<endl;
 }
