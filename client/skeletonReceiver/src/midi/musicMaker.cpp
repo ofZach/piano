310cb3966a5bb85c45ac5de3e6dc62dd5f7efa39
Adam Carlucci <carlucci.adam@gmail.com>
2015-02-06 09:41:24 -0800

    looser drum limit, bias towards kicks

--- client/skeletonReceiver/src/midi/musicMaker.cpp
+++ client/skeletonReceiver/src/midi/musicMaker.cpp
@@ -100,7 +100,8 @@ void musicMaker::setupMidiTriggers() {
 	
 	triggerRef drop = triggerRef(new dropDatNote);
 	midiTrigger::Settings dropSettings(9);
-	dropSettings.notes.push_back(66);
+	dropSettings.notes.push_back(57);
+	dropSettings.notes.push_back(60);
 	drop->setSettings(dropSettings);
 	
 	triggerRef drumStompLeft = triggerRef(new stompNote);
@@ -209,165 +210,165 @@ void musicMaker::updateMidiTriggers(kinectBody &body) {
 
 void musicMaker::updateGraphs(kinectBody &body) {
 
-	for (int i = 0; i < body.historyPlots.size(); i++){
-		if(body.historyPlots[i]->getValues().size()> 0){
-			graphsHistory[i].addSample(body.historyPlots[i]->getValues().back());
-		}
-		
-		if (graphsHistory[i].getTriggered() && body.historyPlots[i]->getValues().size()> 0 ){
-			
-			if((!jazzDrums || historyAndSkeleton)){
-				if(!triggers[body.historyPlots[i]->varName]){
-					
-					if(ofIsStringInString(body.historyPlots[i]->varName, "arm")){
-						skeletonMidi.triggerNote(62+i%2, ofMap(body.historyPlots[i]->getValues().back(),
-															   body.historyPlots[i]->getLowerRange(),
-															   body.historyPlots[i]->getHigerRange(),
-															   0, 127, true));
-						triggers[body.historyPlots[i]->varName] = true;
-					}
-					if(ofIsStringInString(body.historyPlots[i]->varName, "foot-head")){
-						skeletonMidi.triggerNote(i%2==0?77:78, ofMap(body.historyPlots[i]->getValues().back(),
-																	 body.historyPlots[i]->getLowerRange(),
-																	 body.historyPlots[i]->getHigerRange(),
-																	 0, 127, true));
-						triggers[body.historyPlots[i]->varName] = true;
-					}
-					if(ofIsStringInString(body.historyPlots[i]->varName, "elbow-angle")){
-						skeletonMidi.triggerNote(i%2==0?72:73, ofMap(body.historyPlots[i]->getValues().back(),
-																	 body.historyPlots[i]->getLowerRange(),
-																	 body.historyPlots[i]->getHigerRange(),
-																	 0, 127, true));
-						triggers[body.historyPlots[i]->varName] = true;
-					}
-					if(ofIsStringInString(body.historyPlots[i]->varName, "knee-angle")){
-						skeletonMidi.triggerNote(i%2==0?64:65, ofMap(body.historyPlots[i]->getValues().back(),
-																	 body.historyPlots[i]->getLowerRange(),
-																	 body.historyPlots[i]->getHigerRange(),
-																	 0, 127, true));
-						triggers[body.historyPlots[i]->varName] = true;
-					}
-					if(ofIsStringInString(body.historyPlots[i]->varName, "hand-hip")){
-						skeletonMidi.triggerNote(i%2==0?79:80, ofMap(body.historyPlots[i]->getValues().back(),
-																	 body.historyPlots[i]->getLowerRange(),
-																	 body.historyPlots[i]->getHigerRange(),
-																	 0, 127, true));
-						triggers[body.historyPlots[i]->varName] = true;
-					}
-					
-					if(triggers[body.historyPlots[i]->varName]){
-						((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
-                        ((ofApp*)ofGetAppPtr())->TV.addImpluse();
-					}
-					
-				}
-			}
-		}else if(triggers[body.historyPlots[i]->varName]){
-			triggers[body.historyPlots[i]->varName] = false;
-		}
-		graphsHistory[i].setSmoothing(smoothDownHistory, smoothUpHistory);
-		graphsHistory[i].setMinMaxRange(body.historyPlots[i]->getLowerRange(), body.historyPlots[i]->getHigerRange());
-		graphsHistoryThresh[i].set(graphsHistory[i].threshold);
-	}
-	
-	for (int i = 0; i < graphsForSkeleton.size(); i++){
-		if(find(SKELETOR::Instance()->skipList.begin(), SKELETOR::Instance()->skipList.end(),SKELETOR::Instance()->indexToName[i]) == SKELETOR::Instance()->skipList.end()){
-            
-            string nameOfPt = SKELETOR::Instance()->indexToName[i];
-            
-			graphsForSkeleton[i].addSample(body.velLen[i]);
-			if (graphsForSkeleton[i].getTriggered()){
-				if((jazzDrums || historyAndSkeleton)){
-					if(!ofIsStringInString(nameOfPt, "Spine")){
-						if(!triggers[nameOfPt]){
-							
-							if(ofIsStringInString(nameOfPt, "Hand")){
-								skeletonMidi.triggerNote(i%2==0?70:71, graphsForSkeleton[i].getNormalized()*127);
-								triggers[nameOfPt] = true;
-							}
-							if(ofIsStringInString(nameOfPt, "Hip")){
-								skeletonMidi.triggerNote(i%2?68:69, graphsForSkeleton[i].getNormalized()*127);
-								triggers[nameOfPt] = true;
-							}
-							if(ofIsStringInString(nameOfPt, "Foot")){
-								skeletonMidi.triggerNote(i%2?64:65, graphsForSkeleton[i].getNormalized()*127);
-								triggers[nameOfPt] = true;
-							}
-							if(ofIsStringInString(nameOfPt, "Knee")){
-								skeletonMidi.triggerNote(i%2?66:67, graphsForSkeleton[i].getNormalized()*127);
-								triggers[nameOfPt] = true;
-							}
-							if(ofIsStringInString(nameOfPt, "Neck")){
-								skeletonMidi.triggerNote(77, graphsForSkeleton[i].getNormalized()*127);
-								triggers[nameOfPt] = true;
-							}
-							
-							if(triggers[nameOfPt]){
-								ofLog(OF_LOG_NOTICE)<<"Trigger "<<nameOfPt<<endl;
-								((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
-                                ((ofApp*)ofGetAppPtr())->TV.addImpluse();
-
-                            }
-						}
-					}
-					
-				}
-				
-			}else if(triggers[nameOfPt]){
-				triggers[nameOfPt] = false;
-			}
-			graphsForSkeleton[i].setSmoothing(smoothDownHistory, smoothUpHistory);
-			graphsSkeletonThresh[i].set(graphsForSkeleton[i].threshold);
-			
-		}
-	}
-	
-	if(body.gestureHistory.size() > 0){
-		graphs[0].addSample(body.gestureHistory.back()["kick_Left"].value);
-		graphs[1].addSample(body.gestureHistory.back()["kick_Right"].value);
-		graphs[2].addSample(body.gestureHistory.back()["punch_Left"].value);
-		graphs[3].addSample(body.gestureHistory.back()["punch_Right"].value);
-		
-		
-		if(graphs[0].getTriggered()  && !triggers["kick_Left"]){
-			skeletonMidi.triggerKick(60, graphs[0].getNormalized()*127);
-			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
-			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
-			triggers["kick_Left"] = true;
-		}else if(!graphs[0].getTriggered()  && triggers["kick_Left"]){
-			triggers["kick_Left"] = false;
-		}
-		if(graphs[1].getTriggered()  && !triggers["kick_Right"]){
-			skeletonMidi.triggerKick(61, graphs[1].getNormalized()*127);
-			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
-			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
-		}else if(!graphs[1].getTriggered()  && triggers["kick_Right"]){
-			triggers["kick_Right"] = false;
-		}
-		if(graphs[2].getTriggered()&& !triggers["punch_Left"]){
-			skeletonMidi.triggerPunch(62, graphs[2].getNormalized()*127);
-			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
-			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
-		}else if(!graphs[2].getTriggered()  && triggers["punch_Left"]){
-			triggers["punch_Left"] = false;
-		}
-		if(graphs[3].getTriggered() && !triggers["punch_Right"]){
-			skeletonMidi.triggerPunch(63, graphs[3].getNormalized()*127);
-			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
-			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
-		}else if(!graphs[3].getTriggered() && triggers["punch_Right"]){
-			triggers["punch_Right"] = false;
-		}
-		
-		graphs[0].setSmoothing(smoothDownHistory, smoothUpHistory);
-		graphs[1].setSmoothing(smoothDownHistory, smoothUpHistory);
-		graphs[2].setSmoothing(smoothDownHistory, smoothUpHistory);
-		graphs[3].setSmoothing(smoothDownHistory, smoothUpHistory);
-	}
-	
 	for(auto& trig : drumMidiTriggers) {
 		trig->update(body);
 	}
+	
+//	for (int i = 0; i < body.historyPlots.size(); i++){
+//		if(body.historyPlots[i]->getValues().size()> 0){
+//			graphsHistory[i].addSample(body.historyPlots[i]->getValues().back());
+//		}
+//		
+//		if (graphsHistory[i].getTriggered() && body.historyPlots[i]->getValues().size()> 0 ){
+//			
+//			if((!jazzDrums || historyAndSkeleton)){
+//				if(!triggers[body.historyPlots[i]->varName]){
+//					
+//					if(ofIsStringInString(body.historyPlots[i]->varName, "arm")){
+//						skeletonMidi.triggerNote(62+i%2, ofMap(body.historyPlots[i]->getValues().back(),
+//															   body.historyPlots[i]->getLowerRange(),
+//															   body.historyPlots[i]->getHigerRange(),
+//															   0, 127, true));
+//						triggers[body.historyPlots[i]->varName] = true;
+//					}
+//					if(ofIsStringInString(body.historyPlots[i]->varName, "foot-head")){
+//						skeletonMidi.triggerNote(i%2==0?77:78, ofMap(body.historyPlots[i]->getValues().back(),
+//																	 body.historyPlots[i]->getLowerRange(),
+//																	 body.historyPlots[i]->getHigerRange(),
+//																	 0, 127, true));
+//						triggers[body.historyPlots[i]->varName] = true;
+//					}
+//					if(ofIsStringInString(body.historyPlots[i]->varName, "elbow-angle")){
+//						skeletonMidi.triggerNote(i%2==0?72:73, ofMap(body.historyPlots[i]->getValues().back(),
+//																	 body.historyPlots[i]->getLowerRange(),
+//																	 body.historyPlots[i]->getHigerRange(),
+//																	 0, 127, true));
+//						triggers[body.historyPlots[i]->varName] = true;
+//					}
+//					if(ofIsStringInString(body.historyPlots[i]->varName, "knee-angle")){
+//						skeletonMidi.triggerNote(i%2==0?64:65, ofMap(body.historyPlots[i]->getValues().back(),
+//																	 body.historyPlots[i]->getLowerRange(),
+//																	 body.historyPlots[i]->getHigerRange(),
+//																	 0, 127, true));
+//						triggers[body.historyPlots[i]->varName] = true;
+//					}
+//					if(ofIsStringInString(body.historyPlots[i]->varName, "hand-hip")){
+//						skeletonMidi.triggerNote(i%2==0?79:80, ofMap(body.historyPlots[i]->getValues().back(),
+//																	 body.historyPlots[i]->getLowerRange(),
+//																	 body.historyPlots[i]->getHigerRange(),
+//																	 0, 127, true));
+//						triggers[body.historyPlots[i]->varName] = true;
+//					}
+//					
+//					if(triggers[body.historyPlots[i]->varName]){
+//						((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
+//                        ((ofApp*)ofGetAppPtr())->TV.addImpluse();
+//					}
+//					
+//				}
+//			}
+//		}else if(triggers[body.historyPlots[i]->varName]){
+//			triggers[body.historyPlots[i]->varName] = false;
+//		}
+//		graphsHistory[i].setSmoothing(smoothDownHistory, smoothUpHistory);
+//		graphsHistory[i].setMinMaxRange(body.historyPlots[i]->getLowerRange(), body.historyPlots[i]->getHigerRange());
+//		graphsHistoryThresh[i].set(graphsHistory[i].threshold);
+//	}
+//	
+//	for (int i = 0; i < graphsForSkeleton.size(); i++){
+//		if(find(SKELETOR::Instance()->skipList.begin(), SKELETOR::Instance()->skipList.end(),SKELETOR::Instance()->indexToName[i]) == SKELETOR::Instance()->skipList.end()){
+//            
+//            string nameOfPt = SKELETOR::Instance()->indexToName[i];
+//            
+//			graphsForSkeleton[i].addSample(body.velLen[i]);
+//			if (graphsForSkeleton[i].getTriggered()){
+//				if((jazzDrums || historyAndSkeleton)){
+//					if(!ofIsStringInString(nameOfPt, "Spine")){
+//						if(!triggers[nameOfPt]){
+//							
+//							if(ofIsStringInString(nameOfPt, "Hand")){
+//								skeletonMidi.triggerNote(i%2==0?70:71, graphsForSkeleton[i].getNormalized()*127);
+//								triggers[nameOfPt] = true;
+//							}
+//							if(ofIsStringInString(nameOfPt, "Hip")){
+//								skeletonMidi.triggerNote(i%2?68:69, graphsForSkeleton[i].getNormalized()*127);
+//								triggers[nameOfPt] = true;
+//							}
+//							if(ofIsStringInString(nameOfPt, "Foot")){
+//								skeletonMidi.triggerNote(i%2?64:65, graphsForSkeleton[i].getNormalized()*127);
+//								triggers[nameOfPt] = true;
+//							}
+//							if(ofIsStringInString(nameOfPt, "Knee")){
+//								skeletonMidi.triggerNote(i%2?66:67, graphsForSkeleton[i].getNormalized()*127);
+//								triggers[nameOfPt] = true;
+//							}
+//							if(ofIsStringInString(nameOfPt, "Neck")){
+//								skeletonMidi.triggerNote(77, graphsForSkeleton[i].getNormalized()*127);
+//								triggers[nameOfPt] = true;
+//							}
+//							
+//							if(triggers[nameOfPt]){
+//								ofLog(OF_LOG_NOTICE)<<"Trigger "<<nameOfPt<<endl;
+//								((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
+//                                ((ofApp*)ofGetAppPtr())->TV.addImpluse();
+//
+//                            }
+//						}
+//					}
+//					
+//				}
+//				
+//			}else if(triggers[nameOfPt]){
+//				triggers[nameOfPt] = false;
+//			}
+//			graphsForSkeleton[i].setSmoothing(smoothDownHistory, smoothUpHistory);
+//			graphsSkeletonThresh[i].set(graphsForSkeleton[i].threshold);
+//			
+//		}
+//	}
+//	
+//	if(body.gestureHistory.size() > 0){
+//		graphs[0].addSample(body.gestureHistory.back()["kick_Left"].value);
+//		graphs[1].addSample(body.gestureHistory.back()["kick_Right"].value);
+//		graphs[2].addSample(body.gestureHistory.back()["punch_Left"].value);
+//		graphs[3].addSample(body.gestureHistory.back()["punch_Right"].value);
+//		
+//		
+//		if(graphs[0].getTriggered()  && !triggers["kick_Left"]){
+//			skeletonMidi.triggerKick(60, graphs[0].getNormalized()*127);
+//			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
+//			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
+//			triggers["kick_Left"] = true;
+//		}else if(!graphs[0].getTriggered()  && triggers["kick_Left"]){
+//			triggers["kick_Left"] = false;
+//		}
+//		if(graphs[1].getTriggered()  && !triggers["kick_Right"]){
+//			skeletonMidi.triggerKick(61, graphs[1].getNormalized()*127);
+//			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
+//			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
+//		}else if(!graphs[1].getTriggered()  && triggers["kick_Right"]){
+//			triggers["kick_Right"] = false;
+//		}
+//		if(graphs[2].getTriggered()&& !triggers["punch_Left"]){
+//			skeletonMidi.triggerPunch(62, graphs[2].getNormalized()*127);
+//			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
+//			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
+//		}else if(!graphs[2].getTriggered()  && triggers["punch_Left"]){
+//			triggers["punch_Left"] = false;
+//		}
+//		if(graphs[3].getTriggered() && !triggers["punch_Right"]){
+//			skeletonMidi.triggerPunch(63, graphs[3].getNormalized()*127);
+//			((ofApp*)ofGetAppPtr())->floorProjections.triggerTriangles();
+//			((ofApp*)ofGetAppPtr())->floorProjections.addLineTrace();
+//		}else if(!graphs[3].getTriggered() && triggers["punch_Right"]){
+//			triggers["punch_Right"] = false;
+//		}
+//		
+//		graphs[0].setSmoothing(smoothDownHistory, smoothUpHistory);
+//		graphs[1].setSmoothing(smoothDownHistory, smoothUpHistory);
+//		graphs[2].setSmoothing(smoothDownHistory, smoothUpHistory);
+//		graphs[3].setSmoothing(smoothDownHistory, smoothUpHistory);
+//	}
 }
 
 void musicMaker::clearBodies(){
