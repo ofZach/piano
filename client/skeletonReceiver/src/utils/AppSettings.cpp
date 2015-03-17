2381d38fc3ac69ae7996ae2c835801b2d8bece3a
Dan Moore <dan@makeitdoathing.com>
2015-03-17 19:38:09 -0400

    updates to include global settings

---
+++ client/skeletonReceiver/src/utils/AppSettings.cpp
@@ -0,0 +1,9 @@
+//
+//  AppSettings.cpp
+//  sceneView
+//
+//  Created by dantheman on 3/17/15.
+//
+//
+
+#include "AppSettings.h"
---
+++ client/skeletonReceiver/src/utils/AppSettings.h
@@ -0,0 +1,14 @@
+//
+//  AppSettings.h
+//  sceneView
+//
+//  Created by dantheman on 3/17/15.
+//
+//
+
+#ifndef __sceneView__AppSettings__
+#define __sceneView__AppSettings__
+
+#include <stdio.h>
+
+#endif /* defined(__sceneView__AppSettings__) */
