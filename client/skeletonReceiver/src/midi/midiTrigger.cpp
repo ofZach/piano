16f919d4705fa0d4894d9b46d57efe7b657a22a4
Dan Moore <dan@makeitdoathing.com>
2015-03-26 09:08:00 -0400

    miss git stash

--- client/skeletonReceiver/src/midi/midiTrigger.cpp
+++ client/skeletonReceiver/src/midi/midiTrigger.cpp
@@ -1,271 +1,319 @@
 #include "midiTrigger.h"
-
+#include "ofApp.h"
 #pragma mark - Interface
 
 midiTrigger::midiTrigger() : _settings() {
-	
+    
 }
 
 void midiTrigger::setMidiOut(shared_ptr<ofxMidiOut> midiOut) {
-	_midiOut = midiOut;
+    _midiOut = midiOut;
 }
 
 shared_ptr<ofxMidiOut> midiTrigger::getMidiOut() {
-	return _midiOut;
+    return _midiOut;
 }
 
 void midiTrigger::reset() {
     ofLog()<<"reset midi"<<endl;
-	for(const auto& note : getSettings().notes) {
-		getMidiOut()->sendNoteOff(getSettings().channel, note);
-	}
+    for(const auto& note : getSettings().notes) {
+        getMidiOut()->sendNoteOff(getSettings().channel, note);
+    }
 }
 
 #pragma mark - Util
 
 float NormalizedHeight(kinectSkeleton& sk, int name, int side) {
-	if(sk.skeletonHeight == 0) {
-		return 0;
-	} else {
-		ofPoint p = sk.getPoint(name, side);
-		ofPoint f = sk.getPoint(::foot, side);
-		return ofMap(p.distance(f), 0, sk.skeletonHeight * 1.25, 0, 1);
-	}
+    if(sk.skeletonHeight == 0) {
+        return 0;
+    } else {
+        ofPoint p = sk.getPoint(name, side);
+        ofPoint f = sk.getPoint(::foot, side);
+        return ofMap(p.distance(f), 0, sk.skeletonHeight * 1.25, 0, 1);
+    }
 }
 
 template <typename T>
 T ExtractNote(const vector<T>& notes, float perc) {
-	float mx = notes.size() - 1;
-	return notes[ofClamp(perc * mx, 0, mx)];
+    float mx = notes.size() - 1;
+    return notes[ofClamp(perc * mx, 0, mx)];
 }
 
 int ExtractNote(const midiTrigger::Settings &settings, float perc) {
-	return ExtractNote(settings.notes, perc);
+    return ExtractNote(settings.notes, perc);
 }
 
 void AllNotesOff(ofxMidiOut& midiOut) {
-	for(int channel = 1; channel <= 16; channel++) {
-		for(int note = 0; note <= 127; note++) {
-			midiOut.sendNoteOff(channel, note);
-		}
-	}
+    for(int channel = 1; channel <= 16; channel++) {
+        for(int note = 0; note <= 127; note++) {
+            midiOut.sendNoteOff(channel, note);
+        }
+    }
 }
 
 int OtherSide(int side) {
-	return (side == ::left ? ::right : ::left);
+    return (side == ::left ? ::right : ::left);
 }
 
 bool GreatestLength(ofPoint a, ofPoint b) {
-	return a.lengthSquared() > b.lengthSquared();
+    return a.lengthSquared() > b.lengthSquared();
 }
 
 #pragma mark - Grabbed Note
 
 grabbedNote::grabbedNote() : _triggered(false), _currentNote(0) {
-
+    
 }
 
 void grabbedNote::update(kinectBody &body) {
-	kinectSkeleton& sk = body.getLastSkeleton();
-	
-	float extendThreshHi = 0.8;
-	float extendThreshLow = 0.7;
-	
-	float extendPerc = sk.armRightExtendedPct;
-	
-	float height = ofMap(NormalizedHeight(sk, ::hand, ::right), 0.3, 1, 0, 1);
-	int note = ExtractNote(getSettings(), height);
+    kinectSkeleton& sk = body.getLastSkeleton();
     
-	ofVec3f acc = body.accel[SKELETOR::Instance()->rightEnumsToIndex[::hand]];
-	
-	float sum = acc.x + acc.y + acc.z;
-	bool shouldTrig = sum < -3 && extendPerc > extendThreshHi;
-	bool shouldStop = extendPerc < extendThreshLow;
-	
-	if(shouldTrig && !_triggered) {
-		getMidiOut()->sendNoteOn(getSettings().channel, note);
-		_triggered = true;
-		_currentNote = note;
-	} else if(shouldStop && _triggered) {
-		getMidiOut()->sendNoteOff(getSettings().channel, note);
-		_triggered = false;
-		_currentNote = 0;
-	} else if(_triggered && _currentNote != note) {
-		getMidiOut()->sendNoteOn(getSettings().channel, note);
-		getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
-		_currentNote = note;
-	}
+    float extendThreshHi = 0.8;
+    float extendThreshLow = 0.7;
+    
+    float extendPerc = sk.armRightExtendedPct;
+    
+    float height = ofMap(NormalizedHeight(sk, ::hand, ::right), 0.3, 1, 0, 1);
+    int note = ExtractNote(getSettings(), height);
+    
+    ofVec3f acc = body.accel[SKELETOR::Instance()->rightEnumsToIndex[::hand]];
+    
+    float sum = acc.x + acc.y + acc.z;
+    bool shouldTrig = sum < -3 && extendPerc > extendThreshHi;
+    bool shouldStop = extendPerc < extendThreshLow;
+    
+    if(shouldTrig && !_triggered) {
+        getMidiOut()->sendNoteOn(getSettings().channel, note);
+        if(getSettings().channel-8 < 0){
+            ((ofApp*)ofGetAppPtr())->addLineTracePlayerOne();
+        }else{
+            ((ofApp*)ofGetAppPtr())->addLineTracePlayerTwo();
+        }
+        _triggered = true;
+        _currentNote = note;
+    } else if(shouldStop && _triggered) {
+        getMidiOut()->sendNoteOff(getSettings().channel, note);
+        _triggered = false;
+        _currentNote = 0;
+    } else if(_triggered && _currentNote != note) {
+        getMidiOut()->sendNoteOn(getSettings().channel, note);
+        getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
+        _currentNote = note;
+        if(getSettings().channel-8 < 0){
+            ((ofApp*)ofGetAppPtr())->addImpulsePlayerOne();
+        }else{
+            ((ofApp*)ofGetAppPtr())->addImpulsePlayerTwo();
+        }
+    }
 }
 
 #pragma mark - Grid Note
 
 gridNote::gridNote() : _triggered(false), _currentNote(0), _lastTrigger(0) {
-
+    
 }
 
 void gridNote::update(kinectBody &body) {
-	kinectSkeleton& sk = body.getLastSkeleton();
-	
-	float extendPerc;
-	ofVec3f handVel;
-	
-	if(getSettings().side == ::left) {
-		extendPerc = sk.armLeftExtendedPct;
-		handVel = body.velocity[SKELETOR::Instance()->leftEnumsToIndex[::hand]];
-	} else {
-		extendPerc = sk.armRightExtendedPct;
-		handVel = body.velocity[SKELETOR::Instance()->rightEnumsToIndex[::hand]];
-	}
-	
-	float extendThresh = 0.65;
-	bool shouldTrig = extendPerc > extendThresh;
-	int note = ExtractNote(getSettings(), NormalizedHeight(sk, ::hand, getSettings().side));
-	int vel = ofMap(handVel.length(), 0, 40, 40, 120);
-	
-	auto now = ofGetElapsedTimeMillis();
-	unsigned long long timeThresh = 100;
-	bool timeOk = (now - _lastTrigger) > timeThresh;
-	
-	if(shouldTrig && note != _currentNote && timeOk) {
-		getMidiOut()->sendNoteOn(getSettings().channel, note, vel);
-		getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
-		_triggered = true;
-		_currentNote = note;
-		_lastTrigger = now;
-	} else if(!shouldTrig && _triggered) {
-		getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
-		_triggered = false;
-		_currentNote = 0;
-		_lastTrigger = now;
-	}
+    kinectSkeleton& sk = body.getLastSkeleton();
+    
+    float extendPerc;
+    ofVec3f handVel;
+    
+    if(getSettings().side == ::left) {
+        extendPerc = sk.armLeftExtendedPct;
+        handVel = body.velocity[SKELETOR::Instance()->leftEnumsToIndex[::hand]];
+    } else {
+        extendPerc = sk.armRightExtendedPct;
+        handVel = body.velocity[SKELETOR::Instance()->rightEnumsToIndex[::hand]];
+    }
+    
+    float extendThresh = 0.65;
+    bool shouldTrig = extendPerc > extendThresh;
+    int note = ExtractNote(getSettings(), NormalizedHeight(sk, ::hand, getSettings().side));
+    int vel = ofMap(handVel.length(), 0, 40, 40, 120);
+    
+    auto now = ofGetElapsedTimeMillis();
+    unsigned long long timeThresh = 100;
+    bool timeOk = (now - _lastTrigger) > timeThresh;
+    
+    if(shouldTrig && note != _currentNote && timeOk) {
+        getMidiOut()->sendNoteOn(getSettings().channel, note, vel);
+        getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
+        if(getSettings().channel-8 < 0){
+            ((ofApp*)ofGetAppPtr())->addImpulsePlayerOne();
+        }else{
+            ((ofApp*)ofGetAppPtr())->addImpulsePlayerTwo();
+        }
+        
+        _triggered = true;
+        _currentNote = note;
+        _lastTrigger = now;
+    } else if(!shouldTrig && _triggered) {
+        getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
+        _triggered = false;
+        _currentNote = 0;
+        _lastTrigger = now;
+    }
 }
 
 #pragma mark - Accordian Note
 
 accordianNote::accordianNote() : _triggered(false), _currentNote(0), _lastDist(0) {
-	getSettings().notes.resize(2);
+    getSettings().notes.resize(2);
 }
 
 void accordianNote::update(kinectBody &body) {
-	kinectSkeleton& sk = body.getLastSkeleton();
-	
-	float dist = ofMap(sk.getLeftPoint(::hand).distance(sk.getRightPoint(::hand)), 0, sk.skeletonHeight, 0, 1);
-
-	float onThresh = 0.5;
-	float retrigThresh = 0.0004;
-	int note = dist > _lastDist ? getSettings().notes[0] : getSettings().notes[1];
-	
-	if(dist > onThresh) {
-		if(!_triggered) {
-			getMidiOut()->sendNoteOn(getSettings().channel, note);
-		} else if(note != _currentNote && abs(_lastDist - dist) > retrigThresh) {
-			getMidiOut()->sendNoteOn(getSettings().channel, note);
-			getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
-		}
-		_currentNote = note;
-		_triggered = true;
-		_lastDist = dist;
-		
-	} else if(_triggered) {
-		getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
-		_currentNote = 0;
-		_lastDist = 0;
-		_triggered = false;
-	}
+    kinectSkeleton& sk = body.getLastSkeleton();
+    
+    float dist = ofMap(sk.getLeftPoint(::hand).distance(sk.getRightPoint(::hand)), 0, sk.skeletonHeight, 0, 1);
+    
+    float onThresh = 0.5;
+    float retrigThresh = 0.0004;
+    int note = dist > _lastDist ? getSettings().notes[0] : getSettings().notes[1];
+    
+    if(dist > onThresh) {
+        if(!_triggered) {
+            getMidiOut()->sendNoteOn(getSettings().channel, note);
+            if(getSettings().channel-8 < 0){
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerOne();
+            }else{
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerTwo();
+            }
+        } else if(note != _currentNote && abs(_lastDist - dist) > retrigThresh) {
+            getMidiOut()->sendNoteOn(getSettings().channel, note);
+            getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
+            if(getSettings().channel-8 < 0){
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerOne();
+            }else{
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerTwo();
+            }
+        }
+        _currentNote = note;
+        _triggered = true;
+        _lastDist = dist;
+        
+    } else if(_triggered) {
+        getMidiOut()->sendNoteOff(getSettings().channel, _currentNote);
+        _currentNote = 0;
+        _lastDist = 0;
+        _triggered = false;
+    }
 }
 
 #pragma mark - Leg CC
 
 legCC::legCC() : _accumulator(0) {
-	
+    
 }
 
 void legCC::update(kinectBody &body) {
-	kinectSkeleton& sk = body.getLastSkeleton();
-	
-	vector<ofPoint> acc;
-	acc.push_back(body.accel[SKELETOR::Instance()->leftEnumsToIndex[::foot]]);
-	acc.push_back(body.accel[SKELETOR::Instance()->leftEnumsToIndex[::knee]]);
-	acc.push_back(body.accel[SKELETOR::Instance()->leftEnumsToIndex[::hip]]);
-	acc.push_back(body.accel[SKELETOR::Instance()->rightEnumsToIndex[::foot]]);
-	acc.push_back(body.accel[SKELETOR::Instance()->rightEnumsToIndex[::knee]]);
-	acc.push_back(body.accel[SKELETOR::Instance()->rightEnumsToIndex[::hip]]);
-	ofSort(acc, GreatestLength);
-	
-	float greatest = acc.front().length();
-	float amt = ofMap(greatest, 0, sk.skeletonHeight / 32., 0, 1, true);
-	_accumulator = ofLerp(_accumulator, amt, 0.07);
-	getMidiOut()->sendControlChange(1, 1, _accumulator * 127);
+    kinectSkeleton& sk = body.getLastSkeleton();
+    
+    vector<ofPoint> acc;
+    acc.push_back(body.accel[SKELETOR::Instance()->leftEnumsToIndex[::foot]]);
+    acc.push_back(body.accel[SKELETOR::Instance()->leftEnumsToIndex[::knee]]);
+    acc.push_back(body.accel[SKELETOR::Instance()->leftEnumsToIndex[::hip]]);
+    acc.push_back(body.accel[SKELETOR::Instance()->rightEnumsToIndex[::foot]]);
+    acc.push_back(body.accel[SKELETOR::Instance()->rightEnumsToIndex[::knee]]);
+    acc.push_back(body.accel[SKELETOR::Instance()->rightEnumsToIndex[::hip]]);
+    ofSort(acc, GreatestLength);
+    
+    float greatest = acc.front().length();
+    float amt = ofMap(greatest, 0, sk.skeletonHeight / 32., 0, 1, true);
+    _accumulator = ofLerp(_accumulator, amt, 0.07);
+    getMidiOut()->sendControlChange(1, 1, _accumulator * 127);
 }
 
 #pragma mark - Stomp
 
 stompNote::stompNote() {
-	reset();
+    reset();
 }
 
 void stompNote::reset() {
-	_primed = false;
-	_ignoredFrameCount = 0;
+    _primed = false;
+    _ignoredFrameCount = 0;
 }
 
 void stompNote::update(kinectBody &body) {
-	kinectSkeleton& sk = body.getLastSkeleton();
-	
-	float primeThresh = 0.7;
-	float triggerThresh = 0.5;
-	int framesToWait = 40; // number of frames to wait for a negative acceleration
-	
-	size_t idx = SKELETOR::Instance()->getPointIndex(::foot, getSettings().side);
-	size_t opp = SKELETOR::Instance()->getPointIndex(::foot, OtherSide(getSettings().side));
-	
-	float footDiff = ofMap(sk.pts[idx].y - sk.pts[opp].y, 0, sk.skeletonHeight / 10., 0, 1, true);
-	
-	if(!_primed && footDiff > primeThresh) {
-		_primed = true;
-		if(getSettings().notes.size() > 1) {
-			getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes[1]);
-		}
-	} else if(_primed) {
-		
-		float acc = body.accel[idx].y;
-		
-		if(footDiff < triggerThresh) {
-				int velocity = ofMap(acc, -0.1, -4, 80, 100);
-				getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes.front(), velocity);
-				reset();
-		}
-	}
+    kinectSkeleton& sk = body.getLastSkeleton();
+    
+    float primeThresh = 0.7;
+    float triggerThresh = 0.5;
+    int framesToWait = 40; // number of frames to wait for a negative acceleration
+    
+    size_t idx = SKELETOR::Instance()->getPointIndex(::foot, getSettings().side);
+    size_t opp = SKELETOR::Instance()->getPointIndex(::foot, OtherSide(getSettings().side));
+    
+    float footDiff = ofMap(sk.pts[idx].y - sk.pts[opp].y, 0, sk.skeletonHeight / 10., 0, 1, true);
+    
+    if(!_primed && footDiff > primeThresh) {
+        _primed = true;
+        if(getSettings().notes.size() > 1) {
+            getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes[1]);
+            if(getSettings().channel-8 < 0){
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerOne();
+            }else{
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerTwo();
+            }
+        }
+    } else if(_primed) {
+        
+        float acc = body.accel[idx].y;
+        
+        if(footDiff < triggerThresh) {
+            int velocity = ofMap(acc, -0.1, -4, 80, 100);
+            getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes.front(), velocity);
+            reset();
+            if(getSettings().channel-8 < 0){
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerOne();
+            }else{
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerTwo();
+            }
+            
+        }
+    }
 }
 
 #pragma mark - Ass Note
 
 dropDatNote::dropDatNote() : _primed(false), _ignoredFrameCount(0) {
-	
+    
 }
 
 void dropDatNote::reset() {
-	
+    
 }
 
 void dropDatNote::update(kinectBody &body) {
-	kinectSkeleton& sk = body.getLastSkeleton();
-	
-	float primeThresh = 0.95;
-	float triggerThresh = 0.91;
-	
-	float left = sk.legLeftExtendedPct;
-	float right = sk.legRightExtendedPct;
-	bool abovePrime = (left > primeThresh) && (right > primeThresh);
-	bool belowTrigger = (left < triggerThresh) && (right < triggerThresh);
-	
-	if(!_primed && abovePrime) {
-		_primed = true;
-		if(getSettings().notes.size() > 1) {
-			getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes[1]);
-		}
-	} else if(_primed && belowTrigger) {
-		getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes.front());
-		_primed = false;
-	}
+    kinectSkeleton& sk = body.getLastSkeleton();
+    
+    float primeThresh = 0.95;
+    float triggerThresh = 0.91;
+    
+    float left = sk.legLeftExtendedPct;
+    float right = sk.legRightExtendedPct;
+    bool abovePrime = (left > primeThresh) && (right > primeThresh);
+    bool belowTrigger = (left < triggerThresh) && (right < triggerThresh);
+    
+    if(!_primed && abovePrime) {
+        _primed = true;
+        if(getSettings().notes.size() > 1) {
+            getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes[1]);
+            if(getSettings().channel-8 < 0){
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerOne();
+            }else{
+                ((ofApp*)ofGetAppPtr())->addLineTracePlayerTwo();
+            }
+            
+        }
+    } else if(_primed && belowTrigger) {
+        getMidiOut()->sendNoteOn(getSettings().channel, getSettings().notes.front());
+        if(getSettings().channel-8 < 0){
+            ((ofApp*)ofGetAppPtr())->addLineTracePlayerOne();
+        }else{
+            ((ofApp*)ofGetAppPtr())->addLineTracePlayerTwo();
+        }
+        _primed = false;
+    }
 }
